a9c0c290ec9c4cbeff195be91c4155c52d07f717
sacky3105 <kgglee.114.baritone@gmail.com>
2023-10-05 23:43:02 +0900

    Application commit

---
+++ GUISampleApplication/GUISampleApplication.cpp
@@ -0,0 +1,47 @@
+﻿// GUISampleApplication.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "framework.h"
+#include "GUISampleApplication.h"
+
+
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message){
+    case WM_INITDIALOG:
+        return true;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case IDOK:
+            TCHAR szBuf[256];
+            GetDlgItemText(hDlg, IDC_EDIT1, szBuf, _countof(szBuf));
+            OutputDebugString(szBuf);
+            OutputDebugString(TEXT("\n"));
+            return true;
+        case IDCANCEL:
+            EndDialog(hDlg, IDCANCEL);
+            return true;
+        default:
+            return false;
+        }
+    default:
+        return false;
+    }
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    const auto nID =DialogBoxParam
+    (hInstance
+        , MAKEINTRESOURCE(IDD_DIALOG1)
+        , nullptr
+        , DialogProc
+        , 0
+    );
+    return 0;
+}
